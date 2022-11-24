52f024563dd511da63b8d0c42981335f4c35d794
shuhaib s <shuhaib.s@aot-technologies.com>
2022-11-24 15:23:15 +0530

    updated lint error

--- forms-flow-web/src/components/Form/Item/Edit.js
+++ forms-flow-web/src/components/Form/Item/Edit.js
@@ -21,7 +21,7 @@ import {
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { formio_resourceBundles } from "../../../resourceBundles/formio_resourceBundles";
-import { clearFormError } from "../../../actions/formActions";
+import { clearFormError, setFormFailureErrorData } from "../../../actions/formActions";
 import { addTenankey, removeTenantKey } from "../../../helper/helper";
 import { formUpdate } from "../../../apiManager/services/FormServices";
 const reducer = (form, { type, value }) => {
